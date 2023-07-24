a058c3274baba1cc5c24fbc827966bbdd831d820
SamTravie <samnyaga400@gmail.com>
2023-07-25 01:20:00 +0300

    checks for alphabetic character

---
+++ Functions and Nested loops/isalpha.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <ctype.h>
+/*a function that checks for alphabetic character*/
+
+int _isalpha(int c){
+
+    if (isalpha(c) == 1 || isalpha(c) == 2)
+        return (1);
+    else;
+        return (0);
+
+}
+
+int main(void)
+{
+    int r;
+
+    r = _isalpha('H');
+    putchar(r + '0');
+    r = _isalpha('o');
+    putchar(r + '0');
+    r = _isalpha(108);
+    putchar(r + '0');
+    r = _isalpha(';');
+    putchar(r + '0');
+    putchar('\n');
+
+    return (0);
+}
